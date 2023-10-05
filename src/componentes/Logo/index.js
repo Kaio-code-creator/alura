e5f088c3149afa0a951c38f400d5b4181fb37e04
Kaio-code-creator <kaioasousa12@gmail.com>
2023-10-05 20:38:45 -0300

    .

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,16 +1,24 @@
-import './estilo.css'
 import logo from '../../imagens/logo.svg'
+import stytled from 'styled-components'
+
+const LogoContainer = stytled.div `
+    display: flex;
+    font-size: 30px;
+`
+
+const LogoImage = stytled.img `
+    margin-right: 10px;
+`
 
 function Logo () {
     return(
-        <div className='logo'>
-            <img 
+        <LogoContainer>
+            <LogoImage 
             src={logo} 
             alt='logo' 
-            className='logo-img'
-            ></img>
+            />
             <p><strong>Alura</strong>Books</p>
-        </div>
+        </LogoContainer>
     )
 }
 
